ebb3838012a790f00cdd5340c1f1ae418e632e5a
Nicholas de Souza <nrdesouzag13@gmail.com>
2024-01-26 12:36:28 -0500

    fixed cors issues with headers being repeated

--- spotify-gpt/backend/server.mjs
+++ spotify-gpt/backend/server.mjs
@@ -1,21 +1,26 @@
 import dotenv from 'dotenv';
 dotenv.config({ path: './.env' });
+import serverless from 'serverless-http';
+import express from 'express';
+import cors from 'cors';
 
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 const PORT = process.env.PORT || 3001;
 
-import express from 'express';
-import cors from "cors";
-
 const app = express();
 app.use(express.json());
-app.use(cors())
 
+// Use a single CORS middleware with the specific origin
+// app.use(
+//   cors({
+//     origin: 'https://main.d2dkrnlelmb9rv.amplifyapp.com',
+//     methods: 'GET', // Specify the allowed HTTP methods
+//   })
+// );
 
 app.get('/getOpenAIKey', async (req, res) => {
   try {
-    res.json({OPENAI_API_KEY})
-    
+    res.json({ OPENAI_API_KEY });
   } catch (error) {
     console.error('Error sending OPENAI_API_KEY', error);
     res.status(500).json({ error: 'Internal Server Error' });
@@ -26,12 +31,4 @@ app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
 
-
-
-
-
-
-
-
-
-
+export const handler = serverless(app);

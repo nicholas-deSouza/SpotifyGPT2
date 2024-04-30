2462f75f7d6a23d54b48457a5ba26a988cfae9b0
Nicholas de Souza <nrdesouzag13@gmail.com>
2024-04-29 22:54:11 -0400

    updated api key

--- spotify-gpt/backend/server.mjs
+++ spotify-gpt/backend/server.mjs
@@ -1,7 +1,7 @@
-import dotenv from 'dotenv';
-dotenv.config({ path: './.env' });
-import serverless from 'serverless-http';
-import express from 'express';
+import dotenv from "dotenv";
+dotenv.config({ path: "./.env" });
+import serverless from "serverless-http";
+import express from "express";
 import OpenAI from "openai";
 
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
@@ -9,8 +9,7 @@ const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 const app = express();
 app.use(express.json());
 
-app.post('/ListOfSongs', async (req, res) => {
-
+app.post("/ListOfSongs", async (req, res) => {
   try {
     const userInput = req.body.userInput;
 
@@ -19,7 +18,7 @@ app.post('/ListOfSongs', async (req, res) => {
       dangerouslyAllowBrowser: false,
     });
     const initialMessage = `Provide a list of 10 songs related to ${userInput} without any rankings or specific order. Simply provide the names of the songs without including the names of the artists.`;
-    
+
     // openai call
     const chatCompletion = await openai.chat.completions.create({
       model: "gpt-3.5-turbo",
@@ -30,23 +29,21 @@ app.post('/ListOfSongs', async (req, res) => {
         },
       ],
     });
-    
+
     // gets content from openai
     const list = chatCompletion.choices[0].message.content || "";
-    
+
     // Process the list as needed
     // Assuming list is in the format '1. Show Me Love\n2. Finally\n3. Your Love\n...'
-    const gptList = list
-      .split("\n")
-      .map((song) => song.replace(/\d+\./, "").trim());
-    
+    const gptList = list.split("\n").map((song) => song.replace(/\d+\./, "").trim());
+
     // Filter out empty strings or strings that only contain whitespaces
     const filteredList = gptList.filter((song) => song !== "");
-    
+
     res.json({ gptList: filteredList });
   } catch (error) {
-    console.error('Error processing OpenAI Response', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.error("Error processing OpenAI Response", error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
